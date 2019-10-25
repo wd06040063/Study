a5206cd9349337e4f142c27857e8f845fb1ee103
wangdong <1479581973@qq.com>
2019-10-25 14:21:26 +0800

    idea学习笔记——计算器

---
+++ Calculator/src/com/Wang/Demo.java
@@ -0,0 +1,23 @@
+package com.Wang;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+/*
+*快捷键的使用
+*
+* */
+public class Demo {
+    public static void main(String[] args) {
+    String a = "abc";
+    String b = new String("abc");
+    System.out.printf("a=" + a + "b=" + b);
+    ArrayList list = new ArrayList();
+
+    ArrayList list1 = null;
+
+    final Date date = new Date();
+
+
+    }
+}
